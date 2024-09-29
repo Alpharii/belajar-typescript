a165136c9e632120ffef37d3ef94e291c520c73d
alpharii <alphari404@gmail.com>
2024-09-29 10:06:38 +0700

    belajar enums

---
+++ dist/enums.js
@@ -0,0 +1,5 @@
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+var mySize = 2 /* Size.Medium */;
+console.log(mySize);
---
+++ src/enums.ts
@@ -0,0 +1,10 @@
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+//PascalCase
+const enum Size { Small =1 , Medium, Large}
+// pakek const buat kode lebih optimal (const enum)
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
